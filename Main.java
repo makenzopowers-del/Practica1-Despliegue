4101881b664eb4c2a248202f16a58dc008285460
kenzoma1305 <makenzopowers@gmail.com>
2026-06-09 19:44:45 +0200

    saludo modificado en main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Versión Inicial del Proyecto");
+        System.out.println("Versión Inicial del Proyecto - modificao en main");
     }
 
     // Nueva función añadida en la rama-suma
