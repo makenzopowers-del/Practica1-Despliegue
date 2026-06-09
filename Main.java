52e1cef7813165db512cbc095f9c6d4bc8aa1454
kenzoma1305 <makenzopowers@gmail.com>
2026-06-09 19:50:58 +0200

    cambio en main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA BUENAS");
+        System.out.println("HOLA BUENAS (en main)");
     }
 
     // Nueva función añadida en la rama-suma
